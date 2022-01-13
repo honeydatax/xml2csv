6d549f4124b707cb6ea33fc2ff37462c1c55c920
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-01-13 15:13:01 +0000

    ./xml2cvs.sh my.xml

--- xml2cvs.c
+++ xml2cvs.c
@@ -9,6 +9,8 @@ int main(int argc, char *argv[]){
 	char *ccc;
 	char *cccc;
 	FILE* f;
+	int style3=0;
+	int style2=0;
 	int style=0;
 	int r=0;
 		if(argc>1){
@@ -36,27 +38,68 @@ int main(int argc, char *argv[]){
 								cccc=ccc;
 								r=1;
 							}
+							if(style==1 && style2==1)printf(",");
 							if(style==1)printf("%s",cccc);
 							cc=strstr(ccc,"row");
-							if(cc!=NULL && cc-ccc<3)style=1;
+							if(cc!=NULL && cc-ccc<3){
+								style2=0;
+							}
 							cc=strstr(ccc,"ROW");
-							if(cc!=NULL && cc-ccc<3)style=1;
+							if(cc!=NULL && cc-ccc<3){
+								style2=0;
+							}
 							cc=strstr(ccc,"Row");
-							if(cc!=NULL && cc-ccc<3)style=1;
+							if(cc!=NULL && cc-ccc<3){
+								style2=0;
+							}
 
 							cc=strstr(ccc,"/row");
-							if(cc!=NULL && cc-ccc<3)printf("\n");
+							if(cc!=NULL && cc-ccc<3){
+								printf("\n");
+								style2=0;
+								style=0;
+							}
 							cc=strstr(ccc,"/ROW");
-							if(cc!=NULL && cc-ccc<3)printf("\n");
+							if(cc!=NULL && cc-ccc<3){
+								printf("\n");
+								style2=0;
+								style=0;
+							}
 							cc=strstr(ccc,"/Row");
-							if(cc!=NULL && cc-ccc<3)printf("\n");
+							if(cc!=NULL && cc-ccc<3){
+								printf("\n");
+								style2=0;
+								style=0;
+							}
+							
+							cc=strstr(ccc,"Data");
+							if(cc!=NULL && cc-ccc<3){
+								style=1;
+							}
+							cc=strstr(ccc,"DATA");
+							if(cc!=NULL && cc-ccc<3){
+								style=1;
+							}
+							cc=strstr(ccc,"data");
+							if(cc!=NULL && cc-ccc<3){
+								style=1;
+							}
 
-							cc=strstr(ccc,"/cell");
-							if(cc!=NULL && cc-ccc<3)printf(" , ");
-							cc=strstr(ccc,"/CELL");
-							if(cc!=NULL && cc-ccc<3)printf(" , ");
-							cc=strstr(ccc,"/Cell");
-							if(cc!=NULL && cc-ccc<3)printf(" , ");
+							cc=strstr(ccc,"/DATA");
+							if(cc!=NULL && cc-ccc<3){
+								style2=1;
+								style=0;
+							}
+							cc=strstr(ccc,"/data");
+							if(cc!=NULL && cc-ccc<3){
+								style2=1;
+								style=0;
+							}
+							cc=strstr(ccc,"/Data");
+							if(cc!=NULL && cc-ccc<3){
+								style2=1;
+								style=0;
+							}
 
 							cc=strstr(ccc,">");
 							if(cc!=NULL){
